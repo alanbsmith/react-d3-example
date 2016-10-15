9248d389a9387c8c0290fad2dbf6a236915ef668
Alan Smith <a.bax.smith@gmail.com>
2016-10-15 08:47:19 -0600

    initial commit

---
+++ src/ProgressArc.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import d3 from 'd3';
+
+
+class ProgressArc extends Component {
+  render() {
+    return (
+      <div ref="arc"></div>
+    )
+  }
+}
+
+export default ProgressArc;
