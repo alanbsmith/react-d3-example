3b80563f3ceb02cac7d9a5d73d9215f27b9621f9
Alan Smith <a.bax.smith@gmail.com>
2016-10-15 21:53:04 -0600

    adds ProgressArc component that responds to data updates

--- src/App.js
+++ src/App.js
@@ -1,18 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ProgressArc from './ProgressArc';
 
 class App extends Component {
+  constructor(props) {
+     super(props);
+     this.state = {percentComplete: 0.3};
+     this.togglePercent = this.togglePercent.bind(this);
+   }
+
+  togglePercent() {
+    const percentage = this.state.percentComplete === 0.3 ? 0.7 : 0.3;
+    this.setState({percentComplete: percentage});
+  }
+
   render() {
+    console.log(this.state.percentComplete);
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <a onClick={this.togglePercent}>Toggle Arc</a>
+        <ProgressArc
+          height={300}
+          width={300}
+          innerRadius={100}
+          outerRadius={110}
+          id="d3-arc"
+          backgroundColor="#e6e6e6"
+          foregroundColor="#00ff00"
+          percentComplete={this.state.percentComplete}
+        />
       </div>
     );
   }
--- src/ProgressArc.js
+++ src/ProgressArc.js
@@ -1,8 +1,74 @@
-import React, { Component } from 'react';
-import d3 from 'd3';
+import React, { Component, PropTypes } from 'react';
+import * as d3 from "d3";
 
 
 class ProgressArc extends Component {
+  displayName: 'ProgressArc';
+
+  propTypes: {
+    id: PropTypes.string,
+    height: PropTypes.number,
+    width: PropTypes.number,
+    innerRadius: PropTypes.number,
+    outerRadius: PropTypes.number,
+    backgroundColor: PropTypes.string,
+    foregroundColor: PropTypes.string,
+    percentComplete: PropTypes.number
+  }
+
+  componentDidMount() {
+    this.drawArc();
+  }
+
+  componentDidUpdate() {
+    this.redrawArc();
+  }
+
+  drawArc() {
+    const context = this.setContext();
+    this.setBackground(context);
+    this.setForeground(context);
+  }
+
+  redrawArc() {
+    const context = d3.select('#d3-arc');
+    context.remove();
+    this.drawArc();
+  }
+
+  setContext() {
+    const { height, width, id} = this.props;
+    return d3.select(this.refs.arc).append('svg')
+      .attr('height', height)
+      .attr('width', width)
+      .attr('id', id)
+      .append('g')
+      .attr('transform', `translate(${height / 2}, ${width / 2})`);
+  }
+
+  setBackground(context) {
+    return context.append('path')
+    .datum({ endAngle: this.tau })
+    .style('fill', this.props.backgroundColor)
+    .attr('d', this.arc());
+  }
+
+  setForeground(context) {
+    return context.append('path')
+    .datum({ endAngle: this.tau * this.props.percentComplete })
+    .style('fill', this.props.foregroundColor)
+    .attr('d', this.arc());
+  }
+
+  tau = Math.PI * 2;
+
+  arc() {
+    return d3.arc()
+      .innerRadius(this.props.innerRadius)
+      .outerRadius(this.props.outerRadius)
+      .startAngle(0)
+  }
+
   render() {
     return (
       <div ref="arc"></div>
